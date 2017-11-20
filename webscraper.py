ddeccfc5f43fb851aacbae8b9f4abcf8f211f436
Robert Johnk <bobprince.raj@gmail.com>
2017-11-20 14:58:52 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,12 +1,13 @@
 import urllib2
 from bs4 import BeautifulSoup
+pguri = "https://weather.com/weather/today/l/USOR0275:1:US" #URI of the page; feel free to replace it with whatever you like
 
 #First Test
-page = urllib2.urlopen('https://weather.com/weather/today/l/USOR0275:1:US')
+page = urllib2.urlopen(pguri)
 soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find('span', attrs={'classname': 'deg-feels'})
+name_box = soup.find('span', attrs={'classname': 'deg-feels'}) #This identifies the specific html elements you want to inspect
 name = name_box.text.strip()
-print name
+print name #optional
 
 #Wait
 import time
@@ -16,14 +17,14 @@ tm = float(t)
 time.sleep(tm)
 
 #Second Test
-page = urllib2.urlopen('https://weather.com/weather/today/l/USOR0275:1:US')
+page = urllib2.urlopen(pguri)
 soup = BeautifulSoup(page, 'html.parser')
-name_box2 = soup.find('span', attrs={'classname': 'deg-feels'})
+name_box2 = soup.find('span', attrs={'classname': 'deg-feels'}) #Same html elements as above
 name2 = name_box2.text.strip()
 if (name2 != name):
-	print "Text has changed."
-	print name2
+	print "Text has changed." #Make this whatever you like
+	print name2 #optional
 	winsound.Beep(2500, 1000)
 else:
-	print "Text has not changed."
+	print "Text has not changed." #Make this whatever you like
 	winsound.Beep(250, 2000)
