0ad776de256c24a9fca375f1669564480d3a8293
Robert Johnk <bobprince.raj@gmail.com>
2017-11-20 14:28:24 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,17 +1,23 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+#First Test
 page = urllib2.urlopen('https://weather.com/weather/today/l/USOR0275:1:US')
 soup = BeautifulSoup(page, 'html.parser')
 name_box = soup.find('span', attrs={'classname': 'deg-feels'})
 name = name_box.text.strip()
 print name
 
+#Wait
 import time
 import winsound
+t = input ("How long do you want to run the program (in secs)?  ")
+tm = float(t)
+time.sleep(tm)
+
+#Second Test
 page = urllib2.urlopen('https://weather.com/weather/today/l/USOR0275:1:US')
 soup = BeautifulSoup(page, 'html.parser')
-time.sleep(30)
 name_box2 = soup.find('span', attrs={'classname': 'deg-feels'})
 name2 = name_box2.text.strip()
 if (name2 != name):
