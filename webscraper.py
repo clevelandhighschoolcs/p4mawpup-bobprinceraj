8f1e4384c18444fd27f77113a8e7b229a144cceb
Robert Johnk <bobprince.raj@gmail.com>
2017-12-03 12:41:03 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,12 @@
 import urllib2
 from bs4 import BeautifulSoup
-pguri = raw_input("URL/URI of the page you want to inspect:  ")
+pguri = raw_input("URL/URI of page you want to scrape:  ")
 
 #First Test
 page = urllib2.urlopen(pguri)
-soup = BeautifulSoup(page, 'html.parser')
-p_box = soup.find('span', attrs={'p'}) #Inspects the paragraph tags of the page
+soup = BeautifulSoup(page, "html.parser")
+p_box = soup.find("p") #Inspects the paragraph tags of the page
 words = p_box.text.strip()
-print words #optional
 
 #Wait
 import time
@@ -17,12 +16,11 @@ time.sleep(t)
 
 #Second Test
 page = urllib2.urlopen(pguri)
-soup = BeautifulSoup(page, 'html.parser')
-name_box2 = soup.find('span', attrs={'classname': 'deg-feels'}) #Same html elements as above
-name2 = name_box2.text.strip()
-if (name2 != name):
+soup = BeautifulSoup(page, "html.parser")
+p_box2 = soup.find('p') #Same html elements as above
+words2 = p_box2.text.strip()
+if (words2 != words):
 	print "Text has changed." #Make this whatever you like
-	print name2 #optional
 	winsound.Beep(2500, 1000)
 else:
 	print "Text has not changed." #Make this whatever you like
